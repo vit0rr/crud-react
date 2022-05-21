768a356a8fc2fdb26bcd72f7327d885a6a478a56
Vitor <vi.souza.almeida@protonmail.com>
2022-05-20 23:56:37 -0300

    feat: Executing arrow function when clicking out of edit component

--- src/components/EditPhoto.jsx
+++ src/components/EditPhoto.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
+import React from 'react'
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 export const EditPhoto = ({ handleUpdate }) => {
   const [imageUrl, setImageUrl] = useState();
+
   return (
     <>
-      <input
-        type="text"
-        value={imageUrl}
-        placeholder="Paste your new URL image here"
-        onChange={(e) => setImageUrl(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          handleUpdate(imageUrl);
-          setImageUrl('');
-        }}
-      >
-        Send edit
-      </button>
+      <ClickAwayListener onClickAway={() => {console.log("vitor")}}>
+        <input
+          type="text"
+          value={imageUrl}
+          placeholder="Paste your new URL image here"
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        </ClickAwayListener>
+        <button
+          onClick={() => {
+            handleUpdate(imageUrl);
+            setImageUrl("");
+          }}
+        >
+          Send edit
+        </button>
     </>
   );
 };
