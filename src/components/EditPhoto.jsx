34ef6487a8b30d48632bdc4f4786c9af564c4c1d
Vitor <vi.souza.almeida@protonmail.com>
2022-05-23 17:27:07 -0300

    Adding Snackbar alert error

--- src/components/EditPhoto.jsx
+++ src/components/EditPhoto.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 export const EditPhoto = ({ handleUpdate, showInput, shouldShowInput }) => {
   const [imageUrl, setImageUrl] = useState();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(open)
+  }, [open])
+
+  const handleClick = () => {
+    setOpen(true);
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const hideInputClickAway = () => {
     showInput(false);
@@ -14,8 +33,25 @@ export const EditPhoto = ({ handleUpdate, showInput, shouldShowInput }) => {
     return <></>;
   }
 
+  const verifyIfImageUrlIsValid = () => {
+    try {
+      new URL(imageUrl)
+        handleUpdate(imageUrl);
+        setImageUrl("");
+    } catch (error) {
+      handleClick();
+      console.log(error)
+    }
+  };
+
   return (
     <>
+    {/* <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          This is a success message!
+        </Alert>
+      </Snackbar> */} 
+      {open ? <span>Batata</span> : <></>}
       <ClickAwayListener onClickAway={hideInputClickAway}>
         <TextField
           id="outlined-basic"
@@ -29,14 +65,7 @@ export const EditPhoto = ({ handleUpdate, showInput, shouldShowInput }) => {
         variant="contained"
         color="success"
         style={{ margin: "10px" }}
-        onClick={() => {
-          if (new URL(url)) {
-            handleUpdate(imageUrl);
-            setImageUrl("");
-          } else {
-            throw new Error("Invalid URL");
-          }
-        }}
+        onClick={verifyIfImageUrlIsValid}
       >
         Send edit
       </Button>
