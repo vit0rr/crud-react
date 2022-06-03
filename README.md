451c78f1d990ad472756cf0652c4a2573a4171f0
Vitor <vi.souza.almeida@protonmail.com>
2022-06-03 10:30:56 -0300

    README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Run
+
+```shell
+$ git clone https://github.com/vit0rr/crud-react.git
+$ cd crud-react
+$ yarn
+$ yarn dev
+```
+
+<div style="text-align:center">
+
+![screenshot](./media/screenshot.png)
+
+</div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,11 @@ function About() {
       <div className="paragraph">
         <p>
           Currently, this project is a simple crud made with localStorage and
-          ReactJS. The idea is to implement GraphQL and Relay to do the same as
-          I was done with localStorage. You can see my issues and pull requests{' '}
-          <a href="https://github.com/vit0rr/crud-react" target={"_blank"}>here</a>.
+          ReactJS. You can see more about the project{" "}
+          <a href="https://github.com/vit0rr/crud-react" target={"_blank"}>
+            here
+          </a>
+          .
         </p>
       </div>
     </>
